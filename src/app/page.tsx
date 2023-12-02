86b21278062fad1f84f8719c7f6cc60b5ea9a5d2
Vatsal Sakariya <vatsal.sakariya@toddleapp.com>
2023-12-02 22:32:00 +0530

    Fix typo in text alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
         <p
           className={cn(
             typography({ variant: "p" }),
-            "text-cente text-muted-foreground"
+            "text-center text-muted-foreground"
           )}
         >
           SplitIt allows you to split bills more simply while still keeping
